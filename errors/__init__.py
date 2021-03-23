49301c2c14fadd3cf826619d26f443d28e8769b0
Andrej <andrejcarlop@gmail.com>
2021-03-23 14:14:03 +0100

    added custom errors

---
+++ errors/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Package that contains errors that will be used throughout the framework.
+"""
+
+from .input_error import InputAirportError
+
+__all__ = ["InputAirportError"]
---
+++ errors/input_error.py
@@ -0,0 +1,8 @@
+"""
+    Error that indicates that an id or airport name does not exist.
+"""
+
+
+class InputAirportError(Exception):
+    pass
+
