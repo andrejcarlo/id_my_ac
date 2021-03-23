ed4cbe17e2a9b3330e5febdc8a933a3b396f5033
Andrej <andrejcarlop@gmail.com>
2021-03-23 13:19:17 +0100

    working installation of opensky

---
+++ utils/read_opensky.py
@@ -0,0 +1,10 @@
+from opensky_api import OpenSkyApi
+
+
+# read all a/c states in a boundary of switzerland
+
+api = OpenSkyApi()
+# bbox = (min latitude, max latitude, min longitude, max longitude)
+states = api.get_states(bbox=(45.8389, 47.8229, 5.9962, 10.5226))
+for s in states.states:
+    print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
